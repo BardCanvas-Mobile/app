b3b7567532b793e06a108eef84d4942bcb0059c1
Alejandro Caballero <lava.caballero@gmail.com>
2017-12-05 00:34:32 -0600

    Cleanup

--- www/js/helpers/toolbox.js
+++ www/js/helpers/toolbox.js
@@ -55,7 +55,7 @@ var BCtoolbox = {
     {
         if( type !== 'url' ) return false;
         
-        res = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z‌​]{2,6}\b([-a-zA-Z0-9‌​@:%_\+.~#?&=]*)/);
+        res = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z‌​]{2,6}\b([-a-zA-Z0-9‌​@:%_+.~#?&=]*)/);
         return res !== null;
     },
     
